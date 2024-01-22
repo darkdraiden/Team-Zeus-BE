7254298e1cb6eb46be6f5eef417996e9e294d870
Dhruv Bhargav <dhruvbhargav@Dhruvs-MacBook-Pro.local>
2024-01-23 03:17:44 +0530

    User join board functionality

--- backend/Home/models.py
+++ backend/Home/models.py
@@ -1,9 +1,26 @@
 from django.db import models
+
 # Create your models here.
 class User(models.Model):
     user_id=models.AutoField(primary_key=True)
-    user_name=models.CharField(max_length=32)
+    user_name=models.CharField(max_length=32,unique=True)
     first_name=models.CharField(max_length=32)
     last_name=models.CharField(max_length=32)
     password=models.CharField(max_length=32)
     user_email=models.CharField(max_length=32)    
+
+class Board(models.Model):
+    board_id=models.AutoField(primary_key=True)
+    board_name=models.CharField(max_length=32)
+
+class Task(models.Model):
+    task_id=models.AutoField(primary_key=True)
+    task_name=models.CharField(max_length=32)
+    task_desc=models.TextField(max_length=128)
+    time_stamp=models.DateTimeField(auto_now_add=True)
+    task_status=models.CharField(max_length=16)
+    board=models.ForeignKey(Board,on_delete=models.CASCADE,null=True)
+
+class UserBoard(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    board=models.ForeignKey(Board,on_delete=models.CASCADE,null=True)
--- backend/Home/views.py
+++ backend/Home/views.py
@@ -1,6 +1,6 @@
 from rest_framework import status
 from rest_framework.views import APIView,Response
-from Home.models import User
+from Home.models import User,Board,Task,UserBoard
 from Home.serializers import UserSerializer
 
 # Create your views here.
@@ -22,27 +22,71 @@ class UserView(APIView):
             "message": "User added successfully"
         }
         return Response(response,status.HTTP_200_OK)
+    
 class UserLoginView(APIView):
     def post(self,request,user_name):
-            response={}
-            if 'user_name' not in request.data:
-                response['success']=False
-                response['message']='user name required'
-                return Response(response,status.HTTP_400_BAD_REQUEST)
-            user_name=request.data['user_name']
-            password=request.data['password']
-            user=User.objects.filter(user_name=user_name).first()
-            if not user:
-                return Response(response,status.HTTP_401_UNAUTHORIZED)
-            if user_name==user.user_name and password==user.password:
-                response={
-                "success": True,
-                "message": "User successfully authenticated",
+        response={}
+        if 'user_name' not in request.data:
+            response['success']=False
+            response['message']='user name required'
+            return Response(response,status.HTTP_400_BAD_REQUEST)
+        user_name=request.data['user_name']
+        password=request.data['password']
+        user=User.objects.filter(user_name=user_name).first()
+        if not user:
+            return Response(response,status.HTTP_401_UNAUTHORIZED)
+        if user_name==user.user_name and password==user.password:
+            board=Board.objects.all()
+            list=[]
+            for i in board:
+                dict={
+                    "board_id":i.board_id,
+                    "board_name":i.board_name,
+                }
+                list.append(dict)
+            data={
+                "user_name": user_name,
+                "boards": list
             }
-                return Response(response,status.HTTP_200_OK)
-            else:
-                response={
-                "success": False,
-                "message": "Invalid credentials",
+            response={
+            "success": True,
+            "message": "User successfully authenticated",
+            "data": data,
+        }
+            return Response(response,status.HTTP_200_OK)
+        else:
+            response={
+            "success": False,
+            "message": "Invalid credentials",
+        }
+            return Response(response,status.HTTP_401_UNAUTHORIZED)
+
+class AddUsertoBoard(APIView):
+    def post(self,request):
+        response={}
+        if 'user_name' not in request.data:
+            response['success']=False
+            response['message']="Name required"
+            return Response(response,status.HTTP_400_BAD_REQUEST)
+        user_name=request.data['user_name']
+        board_id=request.data['board_id']
+        user_id=User.objects.filter(user_name=user_name).first().user_id
+        UserBoard.objects.create(user_id=user_id,board_id=board_id)
+        list=[]
+        board=Board.objects.all()
+        for i in board:
+                dict={
+                    "board_id":i.board_id,
+                    "board_name":i.board_name,
+                }
+                list.append(dict)
+        data={
+                "user_name": user_name,
+                "boards": list
             }
-                return Response(response,status.HTTP_401_UNAUTHORIZED)
+        response={
+            "success": True,
+            "message": "User joined board",
+            "data":data
+        }
+        return Response(response,status.HTTP_200_OK)
