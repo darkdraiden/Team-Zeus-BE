875d30f3f73260b23951e589808ea63bfaeb071f
Dhruv Bhargav <dhruvbhargav@Dhruvs-MacBook-Pro.local>
2024-01-22 18:36:23 +0530

    signup and login functionality

--- backend/Home/urls.py
+++ backend/Home/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from Home.views import UserView
+from Home.views import UserView,UserLoginView
 
 
 urlpatterns = [
-    path("",UserView.as_view())
+    path("",UserView.as_view()), #for signup page 
+    path("<str:user_name>",UserLoginView.as_view()) #for login page 
 ]
