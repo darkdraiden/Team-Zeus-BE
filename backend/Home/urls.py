b7a318f7bcd27383bd0270eea720a9ab598dd2aa
Dhruv Bhargav <dhruvbhargav@Dhruvs-MacBook-Pro.local>
2024-01-23 21:36:59 +0530

    dashboard functionality

--- backend/Home/urls.py
+++ backend/Home/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from Home.views import UserView,UserLoginView
+from Home.views import UserView,UserLoginView,AddUsertoBoard,AddTaskView,AddBoardView,BoardDetailsView
 
 
 urlpatterns = [
     path("",UserView.as_view()), #for signup page 
-    path("<str:user_name>",UserLoginView.as_view()) #for login page 
+    path("<str:user_name>",UserLoginView.as_view()), #for login page 
+    path("<str:user_name>/dashboard",UserLoginView.as_view()), #for login page 
+    path("<str:user_name>/<int:board_id>",AddUsertoBoard.as_view()), #add user to board
+    path("<str:user_name>/<int:board_id>/addtask",AddTaskView.as_view()), #add task to board
+    path("<str:user_name>/addboard",AddBoardView.as_view()), #add new board
+    path("<str:board_id>",BoardDetailsView.as_view())
 ]
+
--- backend/Home/views.py
+++ backend/Home/views.py
@@ -25,6 +25,7 @@ class UserView(APIView):
     
 class UserLoginView(APIView):
     def post(self,request,user_name):
+        print("in userloginview")
         response={}
         if 'user_name' not in request.data:
             response['success']=False
@@ -52,7 +53,7 @@ class UserLoginView(APIView):
             "success": True,
             "message": "User successfully authenticated",
             "data": data,
-        }
+        }   
             return Response(response,status.HTTP_200_OK)
         else:
             response={
@@ -60,16 +61,30 @@ class UserLoginView(APIView):
             "message": "Invalid credentials",
         }
             return Response(response,status.HTTP_401_UNAUTHORIZED)
+    
+    def get(self,request,user_name):
+        board=Board.objects.all()
+        list=[]
+        for i in board:
+            dict={
+                "board_id":i.board_id,
+                "board_name":i.board_name,
+            }
+            list.append(dict)
+        data={
+            "user_name": user_name,
+            "boards": list
+        }
+        response={
+        "success": True,
+        "message": "User and board details send successfully",
+        "data": data,
+    }   
+        return Response(response,status.HTTP_200_OK)
 
 class AddUsertoBoard(APIView):
-    def post(self,request):
+    def post(self,request,user_name,board_id):
         response={}
-        if 'user_name' not in request.data:
-            response['success']=False
-            response['message']="Name required"
-            return Response(response,status.HTTP_400_BAD_REQUEST)
-        user_name=request.data['user_name']
-        board_id=request.data['board_id']
         user_id=User.objects.filter(user_name=user_name).first().user_id
         UserBoard.objects.create(user_id=user_id,board_id=board_id)
         list=[]
@@ -90,3 +105,65 @@ class AddUsertoBoard(APIView):
             "data":data
         }
         return Response(response,status.HTTP_200_OK)
+
+class AddTaskView(APIView):
+    def post(self,request,user_name,board_id):
+        response={}
+        if 'task_name' not in request.data:
+            response['success']=False
+            response['message']="Need task name"
+            return Response(response,status.HTTP_400_BAD_REQUEST)
+        task_name=request.data['task_name'].replace(" ", "")
+        task_desc=request.data['task_desc']
+        task_status=request.data['task_status']
+        Task.objects.create(task_name=task_name,task_desc=task_desc,task_status=task_status,board_id=board_id)
+        list=[]
+        board=Board.objects.all()
+        for i in board:
+                dict={
+                    "board_id":i.board_id,
+                    "board_name":i.board_name,
+                }
+                list.append(dict)
+        data={
+                "user_name": user_name,
+                "boards": list
+            }
+        response={
+            "success": True,
+            "message": "User joined board",
+            "data":data
+        }
+        return Response(response,status.HTTP_200_OK)
+        
+class AddBoardView(APIView):
+    def post(self,request,user_name):
+        response={}
+        if 'board_name' not in request.data:
+            response['success']=False
+            response['message']="Need board name"
+        board_name=request.data['board_name']
+        Board.objects.create(board_name=board_name)
+        list=[]
+        board=Board.objects.all()
+        for i in board:
+                dict={
+                    "board_id":i.board_id,
+                    "board_name":i.board_name,
+                }
+                list.append(dict)
+        data={
+                "user_name": user_name,
+                "boards": list
+            }
+        response={
+            "success": True,
+            "message": "Board added successfully",
+            "data": data
+
+        }
+        return Response(response,status.HTTP_200_OK)
+
+class BoardDetailsView(APIView):
+     def post(self,request,board_id):
+          pass
